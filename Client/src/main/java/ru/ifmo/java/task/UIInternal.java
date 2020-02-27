949b0be68615aa062545ab04d37fdccfd4eed120
Irina Pluralia <irina-pluralia@rambler.ru>
2020-02-27 18:57:48 +0300

    Add D-metrics

--- Client/src/main/java/ru/ifmo/java/task/UIInternal.java
+++ Client/src/main/java/ru/ifmo/java/task/UIInternal.java
@@ -9,10 +9,12 @@ import java.util.stream.Stream;
 
 public class UIInternal {
     public static void main(String[] args) throws InterruptedException, IOException {
-//        new UIInternal().run(Constants.BLOCKED_SOFT, Arrays.asList(8, 12, 16, 20, 24, 28, 32), 4, 10000, 3000);
+//        new UIInternal().run(Constants.BLOCKED_SOFT,
+//                Arrays.asList(8, 12, 16, 20, 24, 28, 32), 4, 10000, 3000);
 //        new UIInternal().run(Constants.BLOCKED_SOFT, 8, 4,
 //                Arrays.asList(2000, 4000, 6000, 8000, 10000, 12000, 14000), 3000);
-        new UIInternal().run(Constants.BLOCKED_SOFT, 8, 4, 10000, Arrays.asList(0, 500, 1000, 1500, 2000, 2500, 3000));
+        new UIInternal().run(Constants.BLOCKED_SOFT, 8, 4, 10000,
+                Arrays.asList(0, 500, 1000, 1500, 2000, 2500, 3000));
     }
 
     public void run(String archType, List<Integer> clientNum, int taskNum, int taskSize, int sleepTime)
